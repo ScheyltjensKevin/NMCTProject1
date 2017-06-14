435a1992701561be4f6566c4ed46f0f83f8fce68
Scheyltjens_kevin <kevin.scheyltjens@student.howest.be>
2017-06-14 15:48:09 +0200

    changes to main program
    changed the way the LCD updates instead of every 9-10 seconds. it now changes along with the sensor readings.
    added test code for the fans. i can setup a temp as a limit and above it they will turn on, below it they will turn off.
    
    autorun main programm at boot of Raspberry pi.

--- terrariumRegelaar/main.py
+++ terrariumRegelaar/main.py
@@ -8,19 +8,32 @@ l1 = lcd.LCD(24, 27, 25, 22, 13, 19, 26, 12, 16, 20, 21)
 counter = 0
 sleepTime = 3
 
+GPIO.setmode(GPIO.BCM)
+vent = 5
+vent2 = 6
+GPIO.setup(vent,GPIO.OUT)
+GPIO.setup(vent2,GPIO.OUT)
 
 
 try:
+    GPIO.output(vent,GPIO.LOW)
+    GPIO.output(vent2,GPIO.LOW)
     l1.startDisplay()
     while True:
         time.sleep(sleepTime)
         humidity, temperature = dht.readDHT22()
-        if counter == 3:
-            l1.ShowText("Hum: " + humidity + "%")
-            l1.ShowText("Temp: " + temperature + "C")
-            counter = 0
-        counter +=1
+        # if counter == 3:
+        l1.ShowText("Hum: " + humidity + "%")
+        l1.ShowText("Temp: " + temperature + "C")
+        #     counter = 0
+        # counter +=1
 
+        if (float(temperature) > 28.00):
+            GPIO.output(vent,GPIO.HIGH)
+            GPIO.output(vent2,GPIO.HIGH)
+        elif(float(temperature) < 28.00):
+            GPIO.output(vent,GPIO.LOW)
+            GPIO.output(vent2,GPIO.LOW)
 except KeyboardInterrupt:
     l1.reset()
     GPIO.cleanup()

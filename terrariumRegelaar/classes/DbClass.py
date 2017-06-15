c91a81088095156563a9c52418ca533485b2d449
Scheyltjens_kevin <kevin.scheyltjens@student.howest.be>
2017-06-15 20:13:10 +0200

    added code to switch on lamps - at 6 am the day lamp switches on and the night lamp switches of - at 9 pm the day lamp switches off and the night lamp switches onn
    added code to insert data into database and commit.
    -currently at 10 seconds for testing purpose

--- terrariumRegelaar/classes/DbClass.py
+++ terrariumRegelaar/classes/DbClass.py
@@ -12,60 +12,52 @@ class DbClass:
         self.__connection = connector.connect(**self.__dsn)
         self.__cursor = self.__connection.cursor()
 
-    # def getDataFromDatabase(self):
-    #     # Query zonder parameters
-    #
-    #     sqlQuery = "SELECT * FROM tablename"
-    #
-    #     self.__cursor.execute(sqlQuery)
-    #     result = self.__cursor.fetchall()
-    #     self.__cursor.close()
-    #     return result
+    def getDataFromDatabase(self):
+        # Query zonder parameters
 
-    # def getDataFromDatabaseMetVoorwaarde(self, voorwaarde):
-    #     # Query met parameters
-    #     sqlQuery = "SELECT * FROM tablename WHERE columnname = '{param1}'"
-    #     # Combineren van de query en parameter
-    #     sqlCommand = sqlQuery.format(param1=voorwaarde)
-    #
-    #     self.__cursor.execute(sqlCommand)
-    #     result = self.__cursor.fetchall()
-    #     self.__cursor.close()
-    #     return result
+        sqlQuery = "SELECT * FROM tablename"
 
-    def setSensorDataToDatabase(self, sensortype,waarde,timestamp):
+        self.__cursor.execute(sqlQuery)
+        result = self.__cursor.fetchall()
+        self.__cursor.close()
+        return result
+
+    def getDataFromDatabaseMetVoorwaarde(self, voorwaarde):
         # Query met parameters
-        sqlQuery = "INSERT INTO sensordata (sensortype, waarde, timestamp) VALUES ('{param1}', '{param2}','{param3}')"
+        sqlQuery = "SELECT * FROM tablename WHERE columnname = '{param1}'"
+        # Combineren van de query en parameter
+        sqlCommand = sqlQuery.format(param1=voorwaarde)
+
+        self.__cursor.execute(sqlCommand)
+        result = self.__cursor.fetchall()
+        self.__cursor.close()
+        return result
+
+    def setTempDataToDatabase(self, valueTemp, timestamp, fans):
+        if self.__cursor != self.__connection.cursor():
+            self.__cursor = self.__connection.cursor()
+        else:
+            pass
+        # Query met parameters
+        sqlQuery = "INSERT INTO temperature (value,timestamp,fans) VALUES ('{param1}','{param2}','{param3}')"
 
         # Combineren van de query en parameter
-        sqlCommand = sqlQuery.format(param1=sensortype)
-        sqlCommand = sqlQuery.format(param2=waarde)
-        sqlCommand = sqlQuery.format(param3=timestamp)
+        sqlCommand = sqlQuery.format(param1=valueTemp,param2=timestamp,param3=fans)
 
         self.__cursor.execute(sqlCommand)
         self.__connection.commit()
         self.__cursor.close()
 
-    def setlampDataToDatabase(self, lamptype,timestamp):
+    def setHumDataToDatabase(self,valueHum, timestamp):
+        if self.__cursor != self.__connection.cursor():
+            self.__cursor = self.__connection.cursor()
+        else:
+            pass
         # Query met parameters
-        sqlQuery = "INSERT INTO lamp (lamptype,timestamp) VALUES ('{param1}', '{param2}')"
+        sqlQuery = "INSERT INTO humidity (value,timestamp) VALUES ('{param1}','{param2}')"
 
         # Combineren van de query en parameter
-        sqlCommand = sqlQuery.format(param1=lamptype)
-        sqlCommand = sqlQuery.format(param2=timestamp)
-
-        self.__cursor.execute(sqlCommand)
-        self.__connection.commit()
-        self.__cursor.close()
-
-    def setfansDataToDatabase(self, startTime, stopTime,duration):
-        # Query met parameters
-        sqlQuery = "INSERT INTO ventilatoren (startTime, stopTime, duration) VALUES ('{param1}', '{param2}','{param3}')"
-
-        # Combineren van de query en parameter
-        sqlCommand = sqlQuery.format(param1=startTime)
-        sqlCommand = sqlQuery.format(param2=stopTime)
-        sqlCommand = sqlQuery.format(param3=duration)
+        sqlCommand = sqlQuery.format(param1=valueHum, param2=timestamp)
 
         self.__cursor.execute(sqlCommand)
         self.__connection.commit()
--- terrariumRegelaar/main.py
+++ terrariumRegelaar/main.py
@@ -3,6 +3,7 @@ import time
 from classes import DHT22_datareader as dht
 from classes import DbClass as db
 from classes import aLCD_class as lcd
+import datetime
 
 
 l1 = lcd.LCD(24, 27, 25, 22, 13, 19, 26, 12, 16, 20, 21)
@@ -12,35 +13,70 @@ sleepTime = 3
 GPIO.setmode(GPIO.BCM)
 vent = 5
 vent2 = 6
+lampDag = 17
+lampNacht=23
+
 GPIO.setup(vent,GPIO.OUT)
 GPIO.setup(vent2,GPIO.OUT)
+GPIO.setup(lampDag,GPIO.OUT)
+GPIO.setup(lampNacht,GPIO.OUT)
 
+counter=0
+areFansOn = 0
+dataTransferer = db.DbClass()
+start = int(datetime.datetime.now().second)
 
 try:
+    GPIO.output(lampNacht,GPIO.HIGH)
+    GPIO.output(lampDag,GPIO.HIGH)
     GPIO.output(vent,GPIO.LOW)
     GPIO.output(vent2,GPIO.LOW)
     l1.startDisplay()
     while True:
+        dt= datetime.datetime.now()
+
         time.sleep(sleepTime)
         humidity, temperature = dht.readDHT22()
-
-        # db.DbClass.setSensorDataToDatabase(db.DbClass(),1,temperature,time.time())
-        # db.DbClass.setSensorDataToDatabase(db.DbClass(),2,humidity,time.time())
 
         l1.ShowText("Hum: " + humidity + "%")
         l1.ShowText("Temp: " + temperature + "C")
 
-        if (float(temperature) > 28.50):
+        if float(temperature) > 28.50:
             GPIO.output(vent,GPIO.HIGH)
             GPIO.output(vent2,GPIO.HIGH)
-            timestart=time.time()
-        elif(float(temperature) < 27.50):
+            areFansOn = 1
+        elif float(temperature) < 27.50:
             GPIO.output(vent,GPIO.LOW)
             GPIO.output(vent2,GPIO.LOW)
-            timestop=time.time()
-            # db.DbClass.setfansDataToDatabase(db.DbClass(),timestart,timestop,(timestop-timestart))
+            areFansOn = 0
 
+        if dt.hour == 6:
+            GPIO.output(lampDag,GPIO.LOW)
+            GPIO.output(lampNacht,GPIO.HIGH)
+        if dt.hour == 21:
+            GPIO.output(lampDag,GPIO.HIGH)
+            GPIO.output(lampNacht,GPIO.LOW)
 
+        if counter == 0 and (dt.hour > 6 or dt.hour < 21):
+            GPIO.output(lampDag,GPIO.LOW)
+            counter +=1
+        if counter == 0 and (dt.hour < 6 or dt.hour > 21):
+            GPIO.output(lampNacht,GPIO.LOW)
+            counter +=1
+
+        stop = int(datetime.datetime.now().second)
+        result = stop - start
+        # print (stop)
+        # print (start)
+        if result == 10:
+            moment = datetime.datetime.now()
+            dataTransferer.setTempDataToDatabase(temperature,moment,areFansOn)
+            dataTransferer.setHumDataToDatabase(humidity,moment)
+            start = stop
+            print(result)
+        elif stop == 59:
+            stop = 12
+            start = 2
 
 except KeyboardInterrupt:
     l1.reset()

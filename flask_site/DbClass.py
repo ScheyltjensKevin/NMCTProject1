f6b3abe89b6639ddb59c30f6b03425098e97085f
Scheyltjens_kevin <kevin.scheyltjens@student.howest.be>
2017-06-20 09:37:17 +0200

    last commit and push

--- flask_site/DbClass.py
+++ flask_site/DbClass.py
@@ -34,7 +34,7 @@ class DbClass:
 
         # Query zonder parameters
         sqlQuery = "SELECT value FROM temperature"
-
+        self.__connection.reset_session()
         self.__cursor.execute(sqlQuery)
         result = self.__cursor.fetchall()
         self.__cursor.close()
@@ -43,13 +43,14 @@ class DbClass:
 
         latest = len(temp) - 1
 
-        temp = temp[latest - 3] + temp[latest - 2], temp[latest - 10] + temp[latest - 9], \
-                    temp[latest - 17] + temp[latest - 16], \
-                    temp[latest - 24] + temp[latest - 23], temp[latest - 31] + temp[latest - 30], \
-                    temp[latest - 38] + temp[latest - 37], \
-                    temp[latest - 45] + temp[latest - 44], temp[latest - 52] + temp[latest - 51], \
-                    temp[latest - 59] + temp[latest - 58], \
-                    temp[latest - 66] + temp[latest - 65], temp[latest - 73] + temp[latest - 72]
+        temp = temp[latest - 21] + temp[latest - 20], temp[latest - 19] + temp[latest - 18], \
+               temp[latest - 17] + temp[latest - 16], \
+               temp[latest - 15] + temp[latest - 14], temp[latest - 13] + temp[latest - 12], \
+               temp[latest - 11] + temp[latest - 10], \
+               temp[latest - 9] + temp[latest - 8], temp[latest - 7] + temp[latest - 6], \
+               temp[latest - 5] + temp[latest - 4], \
+               temp[latest - 3] + temp[latest -2], temp[latest -1] + temp[latest]
+
 
         temp = str(temp).replace(",","")
         temp = str(temp).replace("(","")
@@ -100,14 +101,15 @@ class DbClass:
 
         latest = len(timestamp) - 1
 
-        timestamp = timestamp[latest - 3] + timestamp[latest - 2], timestamp[latest - 10] + timestamp[latest - 9], \
-                    timestamp[latest - 17] + timestamp[latest - 16], \
-                    timestamp[latest - 24] + timestamp[latest - 23], timestamp[latest - 31] + timestamp[latest - 30], \
-                    timestamp[latest - 38] + timestamp[latest - 37], \
-                    timestamp[latest - 45] + timestamp[latest - 44], timestamp[latest - 52] + timestamp[latest - 51], \
-                    timestamp[latest - 59] + timestamp[latest - 58], \
-                    timestamp[latest - 66] + timestamp[latest - 65], timestamp[latest - 73] + timestamp[latest - 72], \
-                    timestamp[latest - 80] + timestamp[latest - 79]
+        timestamp = timestamp[latest - 73] + timestamp[latest - 72], timestamp[latest - 66] + timestamp[latest - 65], \
+               timestamp[latest - 59] + timestamp[latest - 58], \
+               timestamp[latest - 52] + timestamp[latest - 51], timestamp[latest - 45] + timestamp[latest - 44], \
+               timestamp[latest - 38] + timestamp[latest - 37], \
+               timestamp[latest - 31] + timestamp[latest - 30], timestamp[latest - 24] + timestamp[latest - 23], \
+               timestamp[latest - 17] + timestamp[latest - 16], \
+               timestamp[latest - 10] + timestamp[latest - 9], timestamp[latest - 3] + timestamp[latest - 2]
+
+
 
         timestamp = str(timestamp).replace("' ", "'")
         timestamp = str(timestamp).replace("(", "")
@@ -136,13 +138,13 @@ class DbClass:
 
         latest = len(hum) - 1
 
-        hum = hum[latest - 3] + hum[latest - 2], hum[latest - 10] + hum[latest - 9], \
-              hum[latest - 17] + hum[latest - 16], \
-              hum[latest - 24] + hum[latest - 23], hum[latest - 31] + hum[latest - 30], \
-              hum[latest - 38] + hum[latest - 37], \
-              hum[latest - 45] + hum[latest - 44], hum[latest - 52] + hum[latest - 51], \
-              hum[latest - 59] + hum[latest - 58], \
-              hum[latest - 66] + hum[latest - 65], hum[latest - 73] + hum[latest - 72]
+        hum  = hum[latest - 21] + hum[latest - 20], hum[latest - 19] + hum[latest - 18], \
+               hum[latest - 17] + hum[latest - 16], \
+               hum[latest - 15] + hum[latest - 14], hum[latest - 13] + hum[latest - 12], \
+               hum[latest - 11] + hum[latest - 10], \
+               hum[latest - 9] + hum[latest - 8], hum[latest - 7] + hum[latest - 6], \
+               hum[latest - 5] + hum[latest - 4], \
+               hum[latest - 3] + hum[latest - 2], hum[latest - 1] + hum[latest]
 
         hum = str(hum).replace(",", "")
         hum = str(hum).replace("(", "")
@@ -179,10 +181,14 @@ class DbClass:
         timestamp = str(timestamp).split(',')
 
         latest = len(timestamp)-1
-        timestamp = timestamp[latest-3] + timestamp[latest-2], timestamp[latest-10] + timestamp[latest-9], timestamp[latest-17] + timestamp[latest-16], \
-                    timestamp[latest-24] + timestamp[latest-23], timestamp[latest-31] + timestamp[latest-30], timestamp[latest-38] + timestamp[latest-37], \
-                    timestamp[latest-45] + timestamp[latest-44], timestamp[latest-52] + timestamp[latest-51], timestamp[latest-59] + timestamp[latest-58], \
-                    timestamp[latest - 66] + timestamp[latest - 65], timestamp[latest - 73] + timestamp[latest-72], timestamp[latest-80] + timestamp[latest-79]
+
+        timestamp = timestamp[latest - 73] + timestamp[latest - 72], timestamp[latest - 66] + timestamp[latest - 65], \
+               timestamp[latest - 59] + timestamp[latest - 58], \
+               timestamp[latest - 52] + timestamp[latest - 51], timestamp[latest - 45] + timestamp[latest - 44], \
+               timestamp[latest - 38] + timestamp[latest - 37], \
+               timestamp[latest - 31] + timestamp[latest - 30], timestamp[latest - 24] + timestamp[latest - 23], \
+               timestamp[latest - 17] + timestamp[latest - 16], \
+               timestamp[latest - 10] + timestamp[latest - 9], timestamp[latest - 3] + timestamp[latest - 2]
 
         timestamp = str(timestamp).replace("' ", "'")
         timestamp = str(timestamp).replace("(", "")
